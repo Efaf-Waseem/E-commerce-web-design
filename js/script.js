99d31f0fed52ecfaf84279df5a1554f363f1a82e
Efaf-Waseem <ifafwaseem@gmail.com>
2025-08-10 19:07:23 +0300

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const buttons = document.querySelectorAll(".product-card button");
+    buttons.forEach(button => {
+        button.addEventListener("click", () => {
+            alert("Product added to cart!");
+        });
+    });
+});
